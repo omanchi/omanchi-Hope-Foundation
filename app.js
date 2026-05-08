de2414e2fe05516d97812c65da81bb5a04253ca8
omanchi <samuelomanchi23@gmail.com>
2026-05-08 12:50:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-console.log("Website restored to stable version");
+(function(){
+
+const PAYSTACK_URL = "https://paystack.shop/pay/mrtyf-aq5c";
+
+const EMAILJS_SERVICE_ID = "service_gemvr0r";
+const EMAILJS_TEMPLATE_ID = "template_3dcc8lq";
+const EMAILJS_PUBLIC_KEY = "q_UrmnYn1ewI1H4A_";
+
+emailjs.init(EMAILJS_PUBLIC_KEY);
+
+const form = document.querySelector(".donation-form");
+const amountInput = document.querySelector("input[type='number']");
+const nameInput = document.querySelector("input[type='text']");
+const emailInput = document.querySelector("input[type='email']");
+const buttons = document.querySelectorAll(".donation-options button");
+
+buttons.forEach(btn=>{
+  btn.addEventListener("click",()=>{
+    amountInput.value = btn.innerText;
+  });
+});
+
+form.addEventListener("submit", async (e)=>{
+  e.preventDefault();
+
+  const amount = amountInput.value;
+  const name = nameInput.value;
+  const email = emailInput.value;
+
+  // Unique transaction ID
+  const transactionId = "TXN-" + Date.now();
+
+  try{
+    await emailjs.send(
+      EMAILJS_SERVICE_ID,
+      EMAILJS_TEMPLATE_ID,
+      {
+        to_name: name,
+        to_email: email,
+        amount: amount,
+        transaction_id: transactionId
+      }
+    );
+  }catch(err){
+    console.log("EmailJS error:", err);
+  }
+
+  // Redirect to Paystack
+  window.location.href = PAYSTACK_URL;
+});
+
+})();
